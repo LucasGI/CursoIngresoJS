a1c4422db319a7b952dbc1437422d1e0a4d08482
LucasGI <60513088+LucasGI@users.noreply.github.com>
2020-02-07 14:41:25 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -4,9 +4,7 @@ function mostrar()
 {
 //tomo la edad  
 var edad;
-edad=document.getElementById("edad").value
-if(edad==15) {
-alert("niña bonita")
-
-
-}//FIN DE LA FUNCIÓN
+edad=document.getElementById("edad").value;
+if(edad==15) 
+alert("niña bonita");
+} 
--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,12 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
+edad=document.getElementById("edad").value;
+if(edad>=18) 
+alert("la persona es mayor de edad");
 
+if(edad<18)
+alert("la persona es menor de edad")
 
 }//FIN DE LA FUNCIÓN
