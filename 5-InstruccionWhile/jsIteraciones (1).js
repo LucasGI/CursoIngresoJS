f30af264044694aba0b827136b4c699ab54b605e
LucasGI <60513088+LucasGI@users.noreply.github.com>
2020-02-14 12:03:00 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,13 @@
 function mostrar()
 {
 	alert('iteración while');
-
+/* todo esto no va e un ejemplo de repetir
+	var i;
+while (i<5) {
+	console.log("hols")
+//i=i+1
+	i++;
+	*/
+}
 
 }//FIN DE LA FUNCIÓN
